d82c948b271442d846020144a102d53f3aa8a2ab
can-dev-312 <cannguyen312@gmail.com>
2026-07-26 15:14:03 -0700

    Refactor Two Sum solution to include class structure and type hints

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -1,7 +1,19 @@
-def twoSum(nums: list[int], target: int) -> list[int]:
-    seen = {}
-    for i, num in enumerate(nums):
-        complement = target - num
-        if complement in seen:
-            return [seen[complement], i]
-        seen[num] = i
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+from typing import List
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+# @lc code=end
+
